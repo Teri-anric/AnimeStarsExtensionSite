f2a24586a09f7dbd86f9a2a5408ad5609e1fd101
Teri-anric <2005ahi2005@gmail.com>
2025-06-22 16:40:58 +0300

    Enhance pagination schema with sorting capabilities

--- backend/app/web/schema/pagination.py
+++ backend/app/web/schema/pagination.py
@@ -9,13 +9,20 @@ F = TypeVar("F", bound=BaseEntryFilter)
 S = TypeVar("S")
 
 
+class SortGeneric(BaseSchema, Generic[S]):
+    property: S
+    direction: OrderBy.Sort.Direction
+
+    def to_sort(self) -> OrderBy.Sort:
+        return OrderBy.Sort(property=self.property, direction=self.direction)
+
 class BasePaginationQuery(BaseSchema, Generic[F, S]):
     filter: F | None = Field(default=None, description="Filter by the pagination")
-    order_by: S | None = Field(default=None, description="Order by the pagination")
+    order_by: S | None | list[SortGeneric[S]] = Field(default=None, description="Order by the pagination")
     page: int = 1
     per_page: int = 50
 
-    def build_order_by(self) -> OrderBy | None:
+    def old_build_order_by(self) -> OrderBy | None:
         if self.order_by is None:
             return None
         prop, *args = self.order_by.split(" ")
@@ -23,6 +30,13 @@ class BasePaginationQuery(BaseSchema, Generic[F, S]):
         if "asc" in args:
             direction = OrderBy.Sort.Direction.ASC
         return OrderBy(sorts=[OrderBy.Sort(property=prop, direction=direction)])
+
+    def build_order_by(self) -> OrderBy | None:
+        if isinstance(self.order_by, str):
+            return self.old_build_order_by()
+        if self.order_by is None:
+            return None
+        return OrderBy(sorts=[sort.to_sort() for sort in self.order_by])
 
 
 
