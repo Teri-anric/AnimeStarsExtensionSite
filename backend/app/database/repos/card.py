00cb2416b92cc8723e11a35cc066d68f3f813008
Teri-anric <2005ahi2005@gmail.com>
2025-07-08 22:41:52 +0300

    Enhance card update functionality and filter error handling
    - Updated the card update process in reindex_cards and tasks to include additional fields: name, rank, anime_name, and anime_link, improving data integrity during updates.
    - Refactored ConditionBuilder to raise FilterException for missing fields and unsupported operators, enhancing error handling and robustness in filter processing.
    - Introduced FilterException class for consistent error management across filtering operations.

--- backend/app/database/repos/card.py
+++ backend/app/database/repos/card.py
@@ -3,18 +3,19 @@ from .pagination import PaginationRepository
 from ..models.animestars.card import Card
 from .base import BaseRepository
 from uuid import UUID
+from sqlalchemy import select
 
 
 class CardRepository(
     CRUDRepository[Card, UUID], PaginationRepository[Card], BaseRepository
-):    
+):
     @property
     def entry_class(self) -> type[Card]:
         return Card
-    
+
     async def get_by_card_id(self, card_id: int) -> Card | None:
-        return await self.get_one_by(dict(card_id=card_id))
-    
+        return await self.scalar(select(Card).where(Card.card_id == card_id))
+
     async def get_by_ident(self, ident: int | UUID) -> Card | None:
         if isinstance(ident, int):
             return await self.get_by_card_id(ident)
